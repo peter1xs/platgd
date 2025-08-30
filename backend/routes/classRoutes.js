8167f1a472f452bfa6547467c3827ecc0e7459a1
peter1xs <peterkim120484@gmail.com>
2025-08-30 07:47:25 +0300

    Updated classRoutes.js

--- backend/routes/classRoutes.js
+++ backend/routes/classRoutes.js
@@ -6,14 +6,6 @@ const {
   deleteClass
 } = require("../controllers/classController");
 
-// Debug: Check what's being imported
-const controller = require("../controllers/classController");
-console.log('Available exports from controller:', Object.keys(controller));
-console.log('createClass type:', typeof controller.createClass);
-console.log('verifyClassCode type:', typeof controller.verifyClassCode);
-console.log('getClasses type:', typeof controller.getClasses);
-console.log('deleteClass type:', typeof controller.deleteClass);
-
 const router = express.Router();
 
 router.post("/classCode", createClass);
